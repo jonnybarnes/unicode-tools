c81fa9bda37d20dde6aba03dc5acbe37adc239ed
jonnybarnes <jonnybarnes@gmail.com>
2013-07-08 13:58:14 +0100

    First commit, converts upto 4-byte character codepoints

---
+++ src/Jonnybarnes/UnicodeTools/UnicodeTools.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Jonnybarnes\UnicodeTools;
+
+class UnicodeTools {
+	public function convertUnicodeCodepoints($input) {
+		$output = preg_replace_callback('/\\\\u([0-9a-f]{4,5}\\\\)/i', 'self::utf8CPtoHex', $input);
+		return $output;
+	}
+
+	public function utf8CPtoHex($cp) {
+		$num = $cp[1];
+		$num = '0x' . $num;
+		$bin = base_convert($cp[1], 16, 2);
+		if($num <= 0x7F) { //U+0000 - U+007F -- 1 byte
+			$bin = str_pad($bin, 7, "0", STR_PAD_LEFT);
+			$returnbin = '0' . $bin;
+			$utf8hex = chr('0x' . base_convert($returnbin, 2, 16));
+			return $utf8hex;
+		}
+		if($num <= 0x7FF) { //U+0080 - U+07FF -- 2 bytes
+			$bin = str_pad($bin, 11, "0", STR_PAD_LEFT);
+			$bin1 = substr($bin, 0, 5); $returnbin1 = '110' . $bin1;
+			$bin2 = substr($bin, 5); $returnbin2 = '10' . $bin2;
+			$utf8hex = chr('0x' . base_convert($returnbin1, 2, 16)) . chr('0x' . base_convert($returnbin2, 2, 16));
+			return $utf8hex;
+		}
+		if($num <= 0xFFFF) { //U+0800 - U+FFFF -- 3 bytes
+			$bin = str_pad($bin, 16, "0", STR_PAD_LEFT);
+			$bin1 = substr($bin, 0, 4); $returnbin1 = '1110' . $bin1;
+			$bin2 = substr($bin, 4, 6); $returnbin2 = '10' . $bin2;
+			$bin3 = substr($bin, 10); $returnbin3 = '10' . $bin3;
+			$utf8hex = chr('0x' . base_convert($returnbin1, 2 ,16)) . chr('0x' . base_convert($returnbin2, 2, 16)) . chr('0x' . base_convert($returnbin3, 2, 16));
+			return $utf8hex;
+		}
+		if($num <= 0x1FFFFF) { //U+10000 - U+10FFF -- 4 bytes
+			$bin = str_pad($bin, 21, "0", STR_PAD_LEFT);
+			$bin1 = substr($bin, 0, 3); $returnbin1 = '11110' . $bin1;
+			$bin2 = substr($bin, 3, 6); $returnbin2 = '10' . $bin2;
+			$bin3 = substr($bin, 9, 6); $returnbin3 = '10' . $bin3;
+			$bin4 = substr($bin, 15); $returnbin4 = '10' . $bin4;
+			$utf8hex = chr('0x' . base_convert($returnbin1, 2, 16)) . chr('0x' . base_convert($returnbin2, 2, 16)) . chr('0x' . base_convert($returnbin3, 2, 16)) . chr('0x' . base_convert($returnbin4, 2, 16));
+			return $utf8hex;
+		}
+	}
+}
